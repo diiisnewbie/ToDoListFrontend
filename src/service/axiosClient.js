597d7af27f1ffe716ecc9dbbc1d894c8ddff6e4d
diiisnewbie <nhatduy20072004@gmail.com>
2026-07-06 13:59:56 +0700

    fix baseUrl

--- src/service/axiosClient.js
+++ src/service/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://java-application-1pv9.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
